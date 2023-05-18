c4a74e025ff9c4e7ef3f4514d151dd9f03b72307
Olga-Olga <olga_l@ukr.net>
2023-05-18 15:27:20 +0300

    modal window

---
+++ new-site/js/modal.js
@@ -0,0 +1,22 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-open-modal]"),
+    closeModalBtn: document.querySelector("[data-close-modal]"),
+    backdrop: document.querySelector("[data-backdrop]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  refs.backdrop.addEventListener("click", logBackdropClick);
+
+  function toggleModal() {
+    refs.backdrop.classList.toggle("is-hidden");
+  }
+
+  function logBackdropClick(e) {
+    if (e.target.className !== "backdrop") return;
+
+    refs.backdrop.classList.toggle("is-hidden");
+  }
+})();
